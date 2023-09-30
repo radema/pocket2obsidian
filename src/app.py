43841226853a90beec0d9f6b92b134ae79d21541
Raul De Maio <rauldemaio@gmail.com>
2023-09-30 17:18:10 +0200

    GetPocketData function in app and first print

--- src/app.py
+++ src/app.py
@@ -4,4 +4,24 @@ Script to build a streamlit app
 
 import streamlit as st
 
+import pocket_client as pc
+
+
+def GetPocketData(_p, n_items=10):
+    """
+    Function to manage data retrievel in a single step
+    """
+    access_token = p.makeLoginRequest()
+
+    st.text("Access Done!")
+    data = p.makeRequest(access_token=access_token, n_items=n_items)
+    return data.json()
+
+
 st.title("Pocket Articles to Obsidian")
+
+data_load_state = st.text("Loading data ...")
+p = pc.PocketClient()
+data = GetPocketData(p, n_items=10)
+data_load_state.text("Done!")
+st.write(data)
