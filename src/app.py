8db45bf45697be67a2a70d1b7f8b5e3334c9e870
Raul De Maio <rauldemaio@gmail.com>
2023-09-30 17:32:06 +0200

    print data table in app

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 Script to build a streamlit app
 """
 
+import pandas as pd
 import streamlit as st
 
 import pocket_client as pc
@@ -18,10 +19,22 @@ def GetPocketData(_p, n_items=10):
     return data.json()
 
 
+def TransformPocketData(data):
+    """
+    Select main columns
+    """
+    df = pd.DataFrame.from_dict(data, orient="index")
+    df = df[["item_id", "given_url", "resolved_title", "excerpt"]]
+    df["pocket_url"] = "https://getpocket.com/read/" + df.item_id
+    df.drop(columns="item_id", inplace=True)
+    return df
+
+
 st.title("Pocket Articles to Obsidian")
 
 data_load_state = st.text("Loading data ...")
 p = pc.PocketClient()
 data = GetPocketData(p, n_items=10)
 data_load_state.text("Done!")
-st.write(data)
+df = TransformPocketData(data["list"])
+st.write(df)
